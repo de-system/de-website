e52a523707087e5aaedf27c6ef27e4adfd32b917
j99120701 <helen_lan@gss.com.tw>
2020-12-26 13:26:31 +0800

    修改linebot page table中

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
+//import axios from "axios";
 
 Vue.use(Vuex);
 
@@ -7,17 +8,18 @@ export default new Vuex.Store({
   state: {
     account: "taro",
     cost: "",
+    customer: [],
   },
-  // mutations: {
-  //   fetchCost: (state) => {
-  //     state.cost = "50";
-  //   },
-  // },
-  // actions: {
-  //   fetchCost: async ({ commit }) => {
-  //     //const api = "http://127.0.0.1:3030/cost/cost";
-  //     commit("fetchCost");
-  //   },
-  // },
+  mutations: {
+    fetchCus: (state) => {
+      state.customer = "50";
+    },
+  },
+  actions: {
+    fetchCus: async ({ commit }) => {
+      //const api = "http://127.0.0.1:3030/cost/cost";
+      commit("fetchCost");
+    },
+  },
   getters: {},
 });
--- src/views/linebot.vue
+++ src/views/linebot.vue
@@ -2,6 +2,24 @@
   <div class="linebot">
     <topbar />
     <sidebar />
+    <div id="lineHeader">
+      <span>LineBot 推播</span>
+      <select
+        name=""
+        id="dropdownlist"
+        v-model="formdata.select"
+        @change="change()"
+      >
+        <option value="7">7月</option>
+        <option value="8">8月</option>
+        <option value="9">9月</option>
+        <option value="10">10月</option>
+        <option value="11">11月</option>
+        <option value="12">12月</option>
+      </select>
+    </div>
+
+    <b-table id="btable" striped hover :items="items"></b-table>
   </div>
 </template>
 
@@ -15,11 +33,81 @@ export default {
     topbar,
     sidebar,
   },
+  data() {
+    return {
+      formdata: {
+        select: "",
+      },
+      items: [],
+    };
+  },
+  methods: {
+    change() {
+      this.axios
+        .post("http://127.0.0.1:3030/getLineCus/customer", {
+          month: this.formdata.select,
+        })
+        .then((res) => {
+          if (res.data) {
+            alert(res.data.name.length);
+            // var cusList;
+            for (let i = 1; i < res.data.name.length; i++) {
+              this.items = {
+                姓名: res.data.name[i],
+                顧客年齡: res.data.age[i],
+                line_ID: res.data.lineId[i],
+                日期: res.data.time[i],
+                顧客回應: "",
+              };
+            }
+            alert(this.items);
+          } else alert("回傳錯誤");
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    },
+  },
 };
 </script>
 
 <style scoped>
 .linebot {
   margin: 0%;
+}
+#btable {
+  margin: 0%;
+  top: 27%;
+  left: 20%;
+  position: fixed;
+
+  width: 70vw;
+  height: 60vh;
+  border-radius: 8px;
+  background-color: #d8c0a6;
+  box-shadow: 1px 1px 5px 1px #c3baba;
+  padding: 12px;
+
+  text-align: center;
+  color: white;
+}
+#lineHeader {
+  display: inline-block;
+  margin: 0%;
+  top: 21%;
+  left: 20%;
+  width: 20vw;
+  position: fixed;
+  color: #c9a175;
+  padding-left: 2vw;
+}
+#dropdownlist {
+  margin-left: 2vw;
+  width: 8vw;
+  border: #c3baba;
+  border-radius: 8px;
+  background-color: white;
+  box-shadow: 1px 1px 5px 1px #c3baba;
+  text-align: center;
 }
 </style>
